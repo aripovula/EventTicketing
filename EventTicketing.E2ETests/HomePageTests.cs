4fe85891d2adf34e67db46c223c1d0ccacd585f1
Ula Aripov <ulugbeks@gmail.com>
2026-04-12 10:51:43 -0400

    Setup .NET Playwright E2E tests

---
+++ EventTicketing.E2ETests/HomePageTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace EventTicketing.E2ETests;
+
+[Parallelizable(ParallelScope.Self)]
+public class HomePageTests : PageTest
+{
+    [Test]
+    public async Task HomepageLoads()
+    {
+        await Page.GotoAsync("http://localhost:5173");
+        await Expect(Page).ToHaveTitleAsync("Event Ticketing");
+    }
+}
